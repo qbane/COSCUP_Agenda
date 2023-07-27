15c67c66901d8c734552739aa0645ddc4caa79dc
Andy Pan <andy0130tw@yahoo.com.tw>
2023-07-28 05:10:58 +0800

    Fix service worker

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,21 @@ import vue from '@vitejs/plugin-vue2'
 
 export default defineConfig(async ({ mode }) => {
   return {
+    build: {
+      rollupOptions: {
+        input: {
+          'index': 'index.html',
+          'service_worker': 'src/service_worker.js',
+        },
+        output: {
+          assetFileNames({name}) {
+            if (name == 'index.css') return 'style.css'
+            return '[name][extname]'
+          },
+        },
+      },
+      assetsInlineLimit: 0,
+    },
     plugins: [
       vue({
         template: {

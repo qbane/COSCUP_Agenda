9e69655688c6f3607d45d16dbbc1910d6c7575c5
Andy Pan <andy0130tw@yahoo.com.tw>
2025-08-09 01:17:05 +0800

    Fix avatar again

--- src/components/Agenda.vue
+++ src/components/Agenda.vue
@@ -51,7 +51,7 @@
         v-bind:data-talk-id="talk.id">
         <div style="white-space:nowrap;overflow:hidden;display:flex">
           <span class="talk-time">{{ talk.beginMoment.format('HH:mm') }}–{{ talk.endMoment.format('HH:mm') }}</span>
-          <div class="talk-meta-info" style="min-width:0;overflow:hidden">
+          <div class="talk-meta-info" style="min-width:0;overflow:hidden;flex:1">
             <span class="talk-type" v-if="talk.type.length">{{ talk.type[0] }}</span>
             <span class="talk-tags" v-if="talk.tags.length">
               <span class="talk-tag" v-for="tag in talk.tags">{{ tag }}</span>
